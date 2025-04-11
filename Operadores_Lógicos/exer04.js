719c0741e19adf565d38238209d678498b6a2b4a
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-04-11 11:20:15 -0300

    Ex

---
+++ Operadores_Lógicos/exer04.js
@@ -0,0 +1,4 @@
+let valor1 = true
+let valor2 = false
+resultado = valor1 && valor2
+console.log("o resultado de {valor1} && {valor2} é {resultado}");
---
+++ Operadores_Lógicos/exer05.js
@@ -0,0 +1,6 @@
+let num = 24
+if (num % 2 && num > 10) {
+    console.log("o numero {num} é par e maior que 10");
+} else {
+    console.log("o numero {num} nao é par/ou maior que 10");
+}
